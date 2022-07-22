166d37b1db578497d8def196697bbbf3b4d8f2b6
Krzysztof Nadlonek <knadlonek@gmail.com>
2022-07-22 19:17:16 +0200

    feat: added nav and link animation

--- src/components/AppHeader/AppHeaderDesktop/AppHeaderDesktop.tsx
+++ src/components/AppHeader/AppHeaderDesktop/AppHeaderDesktop.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import CustomLink from '../CustomLink/CustomLink';
 import logo from '../../../../public/assets/logo.png';
+import cx from 'classnames';
 import styles from './AppHeaderDesktop.module.scss';
 
 export const AppHeaderDesktop = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (navRef.current) {
+        const { clientTop, clientHeight, scrollTop, scrollHeight } = navRef.current;
+        // console.log("scroll:", clientTop, clientHeight, scrollTop, scrollHeight, window.scrollY)
+        if (window.scrollY > (clientHeight || scrollHeight)) setScrolled(true);
+        if (window.scrollY < (clientHeight || scrollHeight)) setScrolled(false);
+      }
+    }
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll)
+  }, [])
+
   return (
-    <nav className={styles.container}>
+    <nav className={cx(styles.container, { [styles.scrolledDown]: scrolled })} ref={navRef}>
       <CustomLink href='/' className={styles.logo}>
         <Image src={logo} alt='laureimage logo' />
       </CustomLink>
