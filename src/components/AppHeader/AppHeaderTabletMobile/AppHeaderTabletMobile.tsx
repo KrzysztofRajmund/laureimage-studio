5fde712a17ba563f5a0d83036810a9b41606a570
Krzysztof Nadlonek <knadlonek@gmail.com>
2022-07-23 00:40:25 +0200

    feat: modal basic layout

--- src/components/AppHeader/AppHeaderTabletMobile/AppHeaderTabletMobile.tsx
+++ src/components/AppHeader/AppHeaderTabletMobile/AppHeaderTabletMobile.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
-import Image from 'next/image';
 import Burger from '../Burger/Burger';
 import CustomLink from '../CustomLink/CustomLink';
-import logo from '../../../../public/assets/logo.png';
 import styles from "./AppHeaderTabletMobile.module.scss";
 
 const AppHeaderTabletMobile = () => {
@@ -25,34 +23,32 @@ const AppHeaderTabletMobile = () => {
       <nav className={styles.header}>
         <Burger onClick={toggleModal} show={show} />
         <div className={styles.content}>
-          <h1>Fotografia Ślubna</h1>
+          <h1 className={styles.title}>Fotografia Ślubna</h1>
           <CustomLink href='/' className={styles.logo}>
             LaureImage
           </CustomLink>
         </div>
       </nav>
-      {show && <nav className={styles.modal}>
-        <CustomLink href='/' className={styles.logo}>
-          <Image src={logo} alt='laureimage logo' />
-        </CustomLink>
-        <div className={styles.menu}>
-          <CustomLink href='/'>
-            Home
-          </CustomLink>
-          <CustomLink href='/portfolio'>
-            Portfolio
-          </CustomLink>
-          <CustomLink href='/onas'>
-            O nas
-          </CustomLink>
-          <CustomLink href='/pakiety'>
-            Pakiety
-          </CustomLink>
-          <CustomLink href='/kontakt'>
-            Kontakt
-          </CustomLink>
-        </div>
-      </nav>}
+      {show &&
+        <div className={styles.modal}>
+          <nav className={styles.menu}>
+            <CustomLink href='/'>
+              Home
+            </CustomLink>
+            <CustomLink href='/portfolio'>
+              Portfolio
+            </CustomLink>
+            <CustomLink href='/onas'>
+              O nas
+            </CustomLink>
+            <CustomLink href='/pakiety'>
+              Pakiety
+            </CustomLink>
+            <CustomLink href='/kontakt'>
+              Kontakt
+            </CustomLink>
+          </nav>
+        </div>}
     </>
   )
 };
--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import AppHeader from "../../components/AppHeader";
 import styles from "./Layout.module.scss";
+import AppHeaderTabletMobile from './../../components/AppHeader/AppHeaderTabletMobile/AppHeaderTabletMobile';
 
 export const Layout = ({ children }: any) => {
   return (
-    <div className={styles.container}>
-      <AppHeader />
-      {children}
-      <h1>I am footer</h1>
+    <div className={styles.tabletMobile}>
+      <AppHeaderTabletMobile />
+      <div className={styles.desktop}>
+        {children}
+        <h1>I am footer</h1>
+      </div>
     </div>
   );
 };
