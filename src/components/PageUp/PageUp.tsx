5671000b97519f847d20f1046243bc8d017bde1d
KrzysztofRajmund <knadlonek@gmail.com>
2022-07-26 09:58:32 +0200

    chore: format text,added new size for pageup icon

--- src/components/PageUp/PageUp.tsx
+++ src/components/PageUp/PageUp.tsx
@@ -1,9 +1,11 @@
 import styles from './PageUp.module.scss';
 
 const PageUp = () => {
-    return <div className={styles.container}>
-        <div className={styles.arrow}></div>
-    </div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.arrow}></div>
+        </div>
+    );
 };
 
 export default PageUp;
