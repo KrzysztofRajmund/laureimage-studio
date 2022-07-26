774ceaead16e51db66f0d24454b780f113041ec4
KrzysztofRajmund <knadlonek@gmail.com>
2022-07-26 12:29:55 +0200

    feat: icons + footer links

--- src/components/AppFooter/FooterMiddle/FooterMiddle.tsx
+++ src/components/AppFooter/FooterMiddle/FooterMiddle.tsx
@@ -1,11 +1,23 @@
+import SocialMedia from '../../SocialMedia';
 import styles from './FooterMiddle.module.scss';
 
 const FooterMiddle = () => {
     return (
         <div className={styles.container}>
             <h2>Laureimage</h2>
+            <SocialMedia />
+            <p>
+                Na zdjęciach staram się zawsze zatrzymać dla Państwa chwile i emocje z
+                nimi związane. Jestem tam dla Was tworząc zdjęcia ułożone w historie .
+                Staram się uchwycić ludzi takimi, jacy są. Nie poproszę Was o uśmiech,
+                jestem fanem emocji, ale tych autentycznych, więc wolę poczekać, aż
+                uśmiech przyjdzie sam. Pracując jako fotograf ślubny zawsze dokładam
+                wszelkich starań, aby zdjęcia były ciekawe i niebanalne, zachowując przy
+                edycji naturalne piękne kolory. Jeśli spodobały się Państwu zdjęcia z
+                mojej strony zapraszam do kontaktu w celu poznania pełnej oferty.
+            </p>
         </div>
-    )
-}
+    );
+};
 
-export default FooterMiddle
+export default FooterMiddle;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,11 @@ import type { AppProps } from 'next/app';
 import { Layout } from '../layout/Layout';
 import Root from './root';
 import '../styles/globals.scss';
+//FONTAWESOME FOR NEXTJS:
+//To prevent oversized icons, as they do not use rem: https://fontawesome.com/docs/web/use-with/react/use-with
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
