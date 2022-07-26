9132b457737198586f4e4302d7832aa2b90c6c36
KrzysztofRajmund <knadlonek@gmail.com>
2022-07-26 12:45:14 +0200

    feat: scrollup func

--- src/components/PageUp/PageUp.tsx
+++ src/components/PageUp/PageUp.tsx
@@ -1,8 +1,12 @@
+import { isBrowser } from '../../utils/helpers';
 import styles from './PageUp.module.scss';
 
 const PageUp = () => {
+    const scrollUpHandler = () => {
+        if (isBrowser()) window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
     return (
-        <div className={styles.container}>
+        <div className={styles.container} onClick={scrollUpHandler}>
             <div className={styles.arrow}></div>
         </div>
     );
