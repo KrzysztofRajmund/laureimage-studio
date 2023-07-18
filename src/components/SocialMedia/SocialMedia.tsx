456fec037f520b2bda2a9b3c7c96b1059d54811a
Krzysztof Nadlonek <Krzysztof.Nadlonek@external.bcgdv.com>
2023-07-18 23:25:47 +0200

    feat(er-0718): Adds deploy adjustments

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,28 +1,36 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import styles from './SocialMedia.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagramSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import styles from "./SocialMedia.module.scss";
 
 const SocialMedia = () => {
-
-    return <div className={styles.container}>
-        <a href="https://www.facebook.com/laureimage.weddings/"
-            target="_blank"
-            rel="noreferrer">
-            <FontAwesomeIcon icon={faFacebookF} />
-        </a>
-        <a href="https://www.instagram.com/laureimage/"
-            target="_blank"
-            rel="noreferrer">
-            <FontAwesomeIcon icon={faInstagramSquare} />
-        </a>
-        <a href="mailto:infolaureimage@gmail.com?&subject=LaureImage&body=How can I help?">
-            <FontAwesomeIcon icon={faEnvelope} />
-        </a>
-        <a href="tel:+48-530-003-833">
-            <FontAwesomeIcon icon={faPhone} />
-        </a>
-    </div>;
+  return (
+    <div className={styles.container}>
+      <a
+        href="https://www.facebook.com/laureimage.weddings/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faFacebookF} />
+      </a>
+      <a
+        href="https://www.instagram.com/laureimage/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faInstagramSquare} />
+      </a>
+      <a href="mailto:infolaureimage@gmail.com?&subject=LaureImage&body=How can I help?">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+      <a href="tel:+48-530-003-833">
+        <FontAwesomeIcon icon={faPhone} />
+      </a>
+    </div>
+  );
 };
 
 export default SocialMedia;
