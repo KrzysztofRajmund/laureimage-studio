9f1a9d502cae3067425266af4bcd185c2f1f2595
Krzysztof Nadlonek <Krzysztof.Nadlonek@external.bcgdv.com>
2023-07-19 08:01:27 +0200

    feat(er-0719): Lint ImagesListVertical file

--- src/components/ImagesListVertical/ImagesListVertical.tsx
+++ src/components/ImagesListVertical/ImagesListVertical.tsx
@@ -1,29 +1,29 @@
-import Image from 'next/image';
-import styles from './ImagesListVertical.module.scss';
-import { ImageTypeJSON } from '../../utils/types';
-import { FC } from 'react';
+import Image from "next/image";
+import styles from "./ImagesListVertical.module.scss";
+import { ImageTypeJSON } from "../../utils/types";
+import { FC } from "react";
 
 export interface ImagesListVerticalProps {
-    images: ImageTypeJSON[];
+  images: ImageTypeJSON[];
 }
 
 export const ImagesListVertical: FC<ImagesListVerticalProps> = ({ images }) => {
-
-    return (
-        <div className={styles.container}>
-            {images.map((image: ImageTypeJSON) => {
-                return (
-                    <div key={image.id} className={styles.image}>
-                        <Image
-                            src={`/static/assets/portfolio/${image.url}`}
-                            alt={`image ${image.id}`}
-                            placeholder='blur'
-                            blurDataURL="/static/assets/placeholder.jpg"                            layout='fill'
-                            objectFit='cover'
-                        />
-                    </div>
-                );
-            })}
-        </div >
-    );
+  return (
+    <div className={styles.container}>
+      {images.map((image: ImageTypeJSON) => {
+        return (
+          <div key={image.id} className={styles.image}>
+            <Image
+              src={`/static/assets/portfolio/${image.url}`}
+              alt={`image ${image.id}`}
+              placeholder="blur"
+              blurDataURL="/static/assets/placeholder.jpg"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
